8fad0f46c7e0f0fbc01df84f7019665847416651
nico62497 <nicolas.comes.2105@gmail.com>
2026-01-15 15:57:05 +0100

    Update pipeline.py

---
+++ dags/pipeline.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='airsoft_scraper_pipeline',
+    start_date=datetime(2026, 1, 1),
+    schedule_interval=None, # On le lance manuellement pour tester
+    catchup=False
+) as dag:
+
+    # Étape 1 : Lancer le scraping
+    task_scrape = BashOperator(
+        task_id='run_airsoft_scraper',
+        bash_command='python3 /opt/airflow/scripts/scraper.py'
+    )
+
+    # Étape 2 : Traiter les données avec Spark
+    task_spark = BashOperator(
+        task_id='process_with_spark',
+        bash_command='python3 /opt/airflow/scripts/spark_process.py'
+    )
+
+    # L'ordre : on scrape d'abord, on traite ensuite
+    task_scrape >> task_spark
